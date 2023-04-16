fc9f282464468aeaacce8eb476cbc2719d210da5
HugoPoletto34 <hugopoletto34@gmail.com>
2023-04-16 19:18:08 -0300

    Mudando referencia de Playlist

--- pages/api/videos/youtube.ts
+++ pages/api/videos/youtube.ts
@@ -7,7 +7,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) { 
-  const data = await axios.get(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLAs1mvxndrUhazJ-yCSYjthwePy4b8DzZ&maxResults=20&key=${process.env.YOUTUBE_API_KEY}`);
+  const data = await axios.get(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLAs1mvxndrUiHHydVE4mkonM1Y-oV8L6p&maxResults=20&key=${process.env.YOUTUBE_API_KEY}`);
   const videos = data.data.items.map((item: any) => {
     const result = 'https://www.youtube.com/embed/' + item.snippet.resourceId.videoId
     return {url : result};
